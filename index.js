64e8677c282d1b38bd61379b58970fe77818e74c
guillermo rodriguez <guillermordgz.96@gmail.com>
2022-06-23 19:20:54 -0700

    style changes

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 let header = document.querySelector('#page-header')
 header.style.color = "orange"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+
+let dogNames = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.textAlign = 'left';
+}
+
+
+let footer = document.querySelector('.footer');
+footer.style.color = 'magenta'
+
 // Select some elements...
